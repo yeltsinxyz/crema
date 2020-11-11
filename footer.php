c93249232ca8dfcfed55bd75d2a5bf532747863e
Yeltsin Lima <yeltsin.s.lima@gmail.com>
2020-11-11 09:43:17 -0300

    Adicionado front-page, atualiazado footer

--- footer.php
+++ footer.php
@@ -15,43 +15,41 @@
                     </picture>
                 </a>
 
-                <p class="my-5 leading-relaxed">A <strong>YELTSIN</strong> é uma empresa de desenvolvimento, infraestrutura e marketing digital.</p>
-
-                <p class="my-5 leading-relaxed">Trabalhamos com base em princípios. Transparência, responsabilidade, diversidade, respeito são alguns dos nossos princípios. Por sinal, o tema usado para desenvolver este site está disponível gratuitamente no <a href="https://github.com/yeltsinxyz/crema" target="_blank" class="hover:text-purple-300 underline">GitHub</a>. ;)</p>
+                <p class="my-5 leading-relaxed">A <strong>YELTSIN</strong> é uma empresa de criação de sites, desenvolvimento de aplicativos e softwares, hospedagem de sites e marketing digital. Somos remotos, antes de remoto ser <em>cool</em>.</p>
 
             </div>
 
             <div>
                 
-                <h3 class="text-3xl font-semibold mb-5">Institucional</h3>
+                <h3 class="font-semibold mb-5 uppercase text-purple-200">Institucional</h3>
 
                 <ul class="text-xl">
-                    <li class="pb-2"><a href="https://yeltsin.xyz/" class="text-gray-200 hover:text-purple-400">Home</a></li>
-                    <li class="pb-2"><a href="https://yeltsin.xyz/sobre/" class="text-gray-200 hover:text-purple-400">A Empresa</a></li>
-                    <li class="pb-2"><a href="https://yeltsin.xyz/clientes/" class="text-gray-200 hover:text-purple-400">Clientes</a></li>
-                    <li class="pb-2"><a href="https://yeltsin.xyz/vagas/" class="text-gray-200 hover:text-purple-400">Vagas</a></li>
-                    <li><a href="https://yeltsin.xyz/contato/" class="text-gray-200 hover:text-purple-400">Contato</a></li>
+                    <li class="pb-2"><a href="https://yeltsin.xyz/" class="text-gray-200 hover:text-purple-300">Home</a></li>
+                    <li class="pb-2"><a href="https://yeltsin.xyz/sobre/" class="text-gray-200 hover:text-purple-300">Sobre</a></li>
+                    <li class="pb-2"><a href="https://yeltsin.xyz/clientes/" class="text-gray-200 hover:text-purple-300">Clientes</a></li>
+                    <li class="pb-2"><a href="https://yeltsin.xyz/vagas/" class="text-gray-200 hover:text-purple-300">Vagas</a></li>
+                    <li><a href="https://yeltsin.xyz/contato/" class="text-gray-200 hover:text-purple-300">Contato</a></li>
                 </ul>
 
             </div>
 
             <div>
                 
-                <h3 class="text-3xl font-semibold mb-5">Serviços</h3>
+                <h3 class="font-semibold mb-5 uppercase text-purple-200">Serviços</h3>
 
                 <ul class="text-xl">
-                    <li class="pb-2"><a href="https://yeltsin.xyz/criacao-sites/" class="text-gray-200 hover:text-purple-400">Criação de Sites</a></li>
-                    <li class="pb-2"><a href="https://yeltsin.xyz/criacao-apps/" class="text-gray-200 hover:text-purple-400">Criação de Aplicativos</a></li>
-                    <li class="pb-2"><a href="https://yeltsin.xyz/hospedagem-sites/" class="text-gray-200 hover:text-purple-400">Hospedagem de Sites</a></li>
-                    <li class="pb-2"><a href="https://yeltsin.xyz/servidores-dedicados/" class="text-gray-200 hover:text-purple-400">Servidores Dedicados</a></li>
-                    <li class="pb-2"><a href="https://yeltsin.xyz/midias-sociais/" class="text-gray-200 hover:text-purple-400">Mídias Sociais</a></li>
+                    <li class="pb-2"><a href="https://yeltsin.xyz/criacao-sites/" class="text-gray-200 hover:text-purple-300">Criação de Sites</a></li>
+                    <li class="pb-2"><a href="https://yeltsin.xyz/criacao-apps/" class="text-gray-200 hover:text-purple-300">Criação de Aplicativos</a></li>
+                    <li class="pb-2"><a href="https://yeltsin.xyz/hospedagem-sites/" class="text-gray-200 hover:text-purple-300">Hospedagem de Sites</a></li>
+                    <li class="pb-2"><a href="https://yeltsin.xyz/servidores-dedicados/" class="text-gray-200 hover:text-purple-300">Servidores Dedicados</a></li>
+                    <li class="pb-2"><a href="https://yeltsin.xyz/midias-sociais/" class="text-gray-200 hover:text-purple-300">Mídias Sociais</a></li>
                 </ul>
 
             </div>
 
             <div>
 
-                <h3 class="text-3xl font-semibold mb-5">Newsletter</h3>
+                <h3 class="font-semibold mb-5 uppercase text-purple-200">Newsletter</h3>
 
                 <form method="POST" class="bg-white shadow-md rounded px-4 py-4 mb-4" accept-charset="UTF-8" action="https://www.aweber.com/scripts/addlead.pl">
                 
@@ -64,8 +62,6 @@
                     <input type="hidden" name="meta_required" value="name,email" />
                     <input type="hidden" name="meta_forward_vars" value="1" />
                     <input type="hidden" name="meta_tooltip" value="" />
-
-                    <p class="pb-3 text-purple-800">Preencha o formulário abaixo para receber nossas novidades em seu e-mail. Suas informações <strong>nunca</strong> serão compartilhadas.</p>
 
                     <label class="block text-gray-700 font-bold mb-2" for="awf_field-109469336">
                         Nome:
@@ -91,21 +87,27 @@
 
 <footer id="copyright-info" class="container max-w-full bg-purple-900 text-gray-200 py-6">
 
-    <div class="container mx-auto">
+    <div class="container mx-auto flex flex-col md:flex-row justify-between items-center">
     
-        <div class="grid gap-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2">
+        <p class="text-gray-100 font-medium text-sm text-center md:text-left mb-8 md:mb-0">&copy; 2020 Yeltsin - Todos os direitos reservados</p>
 
-            <div>
+        <div class="flex items-center space-x-6">
 
-                <p class="font-thin text-4xl text-purple-500 py-3">&copy; 2020 Yeltsin</p>
-                
-            </div>
+            <a href="https://github.com/yeltsinxyz/crema" title="GitHub" target="_blank">
 
-            <div>
+                <p class="sr-only">GitHub</p>
             
-                <p class="text-4xl md:text-right lg:text-right py-3"><a href="https://yeltsin.xyz/privacidade/" class="text-purple-300 hover:text-purple-400 underline">Privacidade</a></p>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 2C6.475 2 2 6.475 2 12a9.994 9.994 0 0 0 6.838 9.488c.5.087.687-.213.687-.476 0-.237-.013-1.024-.013-1.862-2.512.463-3.162-.612-3.362-1.175-.113-.288-.6-1.175-1.025-1.413-.35-.187-.85-.65-.013-.662.788-.013 1.35.725 1.538 1.025.9 1.512 2.338 1.087 2.912.825.088-.65.35-1.087.638-1.337-2.225-.25-4.55-1.113-4.55-4.938 0-1.088.387-1.987 1.025-2.688-.1-.25-.45-1.275.1-2.65 0 0 .837-.262 2.75 1.026a9.28 9.28 0 0 1 2.5-.338c.85 0 1.7.112 2.5.337 1.912-1.3 2.75-1.024 2.75-1.024.55 1.375.2 2.4.1 2.65.637.7 1.025 1.587 1.025 2.687 0 3.838-2.337 4.688-4.562 4.938.362.312.675.912.675 1.85 0 1.337-.013 2.412-.013 2.75 0 .262.188.574.688.474A10.016 10.016 0 0 0 22 12c0-5.525-4.475-10-10-10z" fill="rgba(236,240,241,1)"/></svg>
+
+            </a>
+
+            <a href="https://www.linkedin.com/company/yeltsinxyz/" title="LinkedIn" target="_blank">
+
+                <p class="sr-only">LinkedIn</p>
             
-            </div>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32"><path fill="none" d="M0 0h24v24H0z"/><path d="M6.94 5a2 2 0 1 1-4-.002 2 2 0 0 1 4 .002zM7 8.48H3V21h4V8.48zm6.32 0H9.34V21h3.94v-6.57c0-3.66 4.77-4 4.77 0V21H22v-7.93c0-6.17-7.06-5.94-8.72-2.91l.04-1.68z" fill="rgba(236,240,241,1)"/></svg>
+
+            </a>
 
         </div>
 
